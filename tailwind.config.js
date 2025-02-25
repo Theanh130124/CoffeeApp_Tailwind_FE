65784a714a98a63a26742b6335c0fd1370611c96
Theanh130124 <theanhtran13012004@gmail.com>
2025-02-25 15:19:39 +0700

    Update Layout Basic Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
  export default {
        content: ["./src/**/*.{html,js}"],
         theme: {
-          extend: {},
+          extend: {
+            colors : {
+              //Các này mình tự định nghĩa chứ tailwind Không có
+              'light-coffee': "#C89F94"
+            }
+
+          },
         },
         plugins: [],
       }
